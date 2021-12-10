57d724a3f1ecf67cf9a761b15e733235ea37f6da
Vladimir <korshuk.vladimir@gmail.com>
2021-12-10 17:48:34 +0200

    created POST table

--- runserver.py
+++ runserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 
 app = Flask(__name__)
@@ -9,9 +9,19 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/chart-links')
-def chart_links():
+@app.route('/chart-links', methods=["GET"])
+def chart_links_get():
     return render_template("chart-links.html")
+
+
+@app.route('/chart-links', methods=["POST"])
+def chart_links_post():
+    url = {"nice": "awesome", "dick": "balls"}
+    url_items = url.items()
+    # product_links = url.values()
+    # url = request.form['urlInput']
+
+    return render_template("chart-links.html", url_items=url_items)
 
 
 if __name__ == "__main__":
